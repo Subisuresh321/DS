f77c94b82a2db484f4dca275d748114cdda4dc66
Subisuresh321 <subisuresh69@gmail.com>
2024-12-09 16:18:53 +0530

    error free commit

--- bst.c
+++ bst.c
@@ -8,20 +8,7 @@ struct node
     struct node *right;
 };
 
-struct node *root = NULL, *leaf, *newnode,*parent;
-
-void create()
-{
-    int num;
-    printf("\nEnter the initial numbers of nodes:");
-    scanf("%d", &num);
-    int i = 0;
-    while (i < num)
-    {
-        insert();
-        i++;
-    }
-}
+struct node *root = NULL, *leaf, *newnode, *parent;
 
 void insert()
 {
@@ -37,18 +24,49 @@ void insert()
     else
     {
         leaf = root;
-        parent=NULL;
-        while (leaf!=NULL)
+        parent = NULL;
+        while (leaf != NULL)
         {
-            parent=leaf;
+            parent = leaf;
             if (newnode->data < leaf->data)
                 leaf = leaf->left;
             else
-                leaf=leaf->right;
+                leaf = leaf->right;
         }
-        if(newnode->data<parent->data)
-        parent->left=newnode;
+        if (newnode->data < parent->data)
+            parent->left = newnode;
         else
-        parent->right=newnode;
+            parent->right = newnode;
     }
+}
+
+
+void create()
+{
+    int num;
+    printf("\nEnter the initial numbers of nodes:");
+    scanf("%d", &num);
+    int i = 0;
+    while (i < num)
+    {
+        insert();
+        i++;
+    }
+}
+
+
+void inorder(struct node *root)
+{
+    if (root != NULL)
+    {
+        inorder(root->left);
+        printf("%d", root->data);
+        inorder(root->right);
+    }
+}
+
+void main()
+{
+    create();
+    inorder(root);
 }
