00dbc2b1c3676256ca04530ddd7d24bc5b876746
unknown <subisuresh69@gmail.com>
2024-12-09 08:23:23 +0530

    monor changes with errors

--- bst1.c
+++ bst1.c
@@ -8,7 +8,7 @@ struct node
     struct node *right;
 };
 
-struct node *root = NULL, *parent = NULL;
+struct node *root = NULL, *parent = NULL,*succParent=NULL;
 void create();
 void insert();
 struct node *insertEle(struct node *root, struct node *newnode);
@@ -24,7 +24,8 @@ void main()
     create();
     display(root);
     search();
-    delete();
+    delete ();
+    delete ();
 }
 
 void create()
@@ -72,7 +73,7 @@ void display(struct node *root)
 void search()
 {
     int ele;
-    printf("Enter the element to search: \n");
+    printf("\nEnter the element to search: \n");
     scanf("%d", &ele);
     if (searchEle(root, ele) != NULL)
     {
@@ -97,20 +98,28 @@ struct node *searchEle(struct node *root, int ele)
 
 void delete()
 {
-    struct node *temp;
-    int ele;
-    printf("Enter the element to delete: \n");
-    scanf("%d", &ele);
-    temp = searchEle(root, ele);
-    if (temp == NULL)
+    if (root == NULL)
     {
-        printf("The element %d not found\n", ele);
+        printf("No element to delete\n");
+        return;
     }
     else
     {
-        deleteEle(temp);
-        printf("The element %d Deleted\n", ele);
-        display(root);
+        struct node *temp;
+        int ele;
+        printf("Enter the element to delete: \n");
+        scanf("%d", &ele);
+        temp = searchEle(root, ele);
+        if (temp == NULL)
+        {
+            printf("The element %d not found\n", ele);
+        }
+        else
+        {
+            deleteEle(temp);
+            printf("The element %d Deleted\n", ele);
+            display(root);
+        }
     }
 }
 
@@ -122,53 +131,36 @@ void deleteEle(struct node *temp)
         {
             root = NULL;
         }
-        if (parent->left == temp)
+        else if (parent->left == temp)
             parent->left = NULL;
         else
             parent->right = NULL;
         free(temp);
     }
     else if (temp->left != NULL && temp->right != NULL)
-    {
-        struct node *succ = findSucc(temp);
-        succ = searchEle(root, succ->data);
+    {   
+        struct node *succ = findSucc(temp->right); 
+
         temp->data = succ->data;
-        if (succ->right != NULL)
-        {
-            parent->left = succ->right;
-            free(succ);
-        }
-        else
-        {
-            parent->left = NULL;
-            free(succ);
-        }
+        parent=succParent;
+        deleteEle(succ);
     }
     else
     {
-        if (parent->left == temp)
-        {
-            if (temp->left != NULL)
-                parent->left = temp->left;
-            else
-                parent->left = temp->right;
-        }
-        else
-        {
-            if (temp->left != NULL)
-                parent->right = temp->left;
-            else
-                parent->right = temp->right;
-        }
-        free(temp);
+        struct node *child = temp->left != NULL ? temp->left : temp->right;
+        temp->data = child->data;
+        temp->left = child->left;
+        temp->right = child->right;
+        free(child);
     }
 }
 
 struct node *findSucc(struct node *temp)
 {
-    temp = temp->right;
+    succParent=temp;
     while (temp->left != NULL)
     {
+        succParent = temp;  
         temp = temp->left;
     }
     return temp;
