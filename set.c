db5dbbfcb1205969c664686722e480e657b10c46
Subisuresh321 <subisuresh69@gmail.com>
2024-12-10 22:45:45 +0530

    set completed,bfs added

--- set.c
+++ set.c
@@ -1,22 +1,22 @@
 #include<stdio.h>
 #include<stdlib.h>
-void set_union(int arr1[],int arr2[],int limit)
+void set_union(int arr1[],int arr2[],int u[],int limit)
 {
     printf("\n The Union is:{");
     for(int i=0;i<limit;i++)
     {
-        if(arr1[i] || arr2[i]==1)
-        printf("%d,",i);
+        if(arr1[i]==1 || arr2[i]==1)
+        printf("%d,",u[i]);
     }
     printf("}");
 }
-void set_intersection(int arr1[],int arr2[],int limit)
+void set_intersection(int arr1[],int arr2[],int u[],int limit)
 {
     printf("\n The Intersection is:{");
     for(int i=0;i<limit;i++)
     {
-        if(arr1[i] && arr2[i]==1)
-        printf("%d,",i);
+        if(arr1[i]==1 && arr2[i]==1)
+        printf("%d,",u[i]);
     }
     printf("}");
 }
@@ -25,16 +25,41 @@ void set_display(int arr[],int limit)
 {
     printf("\n The Set is:{");
     for(int i=0;i<limit;i++)
-    if(arr[i]==1)
-    printf("%d",arr[i]);
+    printf("%d,",arr[i]);
     printf("}");
 }
+// void set_display(int arr[],int limit)
+// {
+//     printf("\n The Set is:{");
+//     for(int i=0;i<limit;i++)
+//     if(arr[i]==1)
+//     printf("%d",arr[i]);
+//     printf("}");
+// }
 
+int isPresent(int data,int u[],int limit)
+{
+    int i=0;
+    while (i<limit)
+    {
+        if(data==u[i])
+        return i;
+        i++;
+    }
+    return -1; 
+}
 void main()
 {
     int limit,num1,num2;
     printf("\nEnter the number of elements in universal set:");
     scanf("%d",&limit);
+    int u[limit];
+    printf("\nEnter the elements of universal set:");
+    for(int i=0;i<limit;i++){
+    printf("\nEnter the Element:");    
+    scanf("%d",&u[i]);
+    }
+    set_display(u,limit);
     int arr1[limit],arr2[limit];
 
     for(int i=0;i<limit;i++)
@@ -42,43 +67,49 @@ void main()
         arr1[i]=arr2[i]=0;
     }
     
-    printf("\nEnter the elements of first subset:");
+    printf("\nEnter the elements of first subset(less than %d):",limit);
     scanf("%d",&num1);
     int i=0;
     while(i<num1)
     {
-        int data;
+        int k,data;
         printf("\nEnter the Element:");
         scanf("%d",&data);
-        if(data>=limit || data<0)
-        printf("\nEnter a valid element");
+        k=isPresent(data,u,limit);
+        if(k!=-1)
+        {
+            arr1[k]=1;
+            i++;
+        }
         else
         {
-            arr1[data]=1;
-            i++;
+            printf("\nEnter a valid element which is a subset of Universal Set");
         }
 
     }
     
-    printf("\nEnter the elements of second subset:");
+    printf("\nEnter the elements of second subset(less than %d):",limit);
     scanf("%d",&num2);
     i=0;
     while(i<num2)
     {
-        int data;
+        int k,data;
         printf("\nEnter the Element:");
         scanf("%d",&data);
-        if(data>=limit || data<0)
-        printf("\nEnter a valid element");
+        k=isPresent(data,u,limit);
+        if(k!=-1)
+        {
+            arr2[k]=1;
+            i++;
+        }
         else
         {
-            arr2[data]=1;
-            i++;
+            printf("\nEnter a valid element which is a subset of Universal Set");
         }
 
     }
 set_display(arr1,limit);
 set_display(arr2,limit);
-set_union(arr1,arr2,limit);
-set_intersection(arr1,arr2,limit);
+set_union(arr1,arr2,u,limit);
+set_intersection(arr1,arr2,u,limit);
 }
