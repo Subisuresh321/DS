472085f810840b991061d10d43b2c980449c3b5c
Subisuresh321 <subisuresh69@gmail.com>
2024-12-12 18:12:22 +0530

    bfs completed

--- bfs.c
+++ bfs.c
@@ -1,38 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
-void BFS(int graph[][], int num)
+#define max 100
+void BFS(int graph[][max], int num)
 {
     int visited[num], queue[num];
     int front = 0, rear = 0;
-    for(int i=0;i<num;i++)
-    visited[i]=0;
-    int s=0;
-    while(front<rear)
+    for (int i = 0; i < num; i++)
+        visited[i] = 0;
+    int s = 0;
+    visited[s] = 1;
+    queue[rear++] = s;
+    while (front < rear)
     {
-        visited[s]=1;
-        queue[rear++]=s;
+        s=queue[front++];
+        for (int i = 0; i < num; i++)
+        {
+            if (graph[s][i] == 1)
+            {
+                if (visited[i] == 0)
+                {
+                    queue[rear++] = i;
+                    visited[i] = 1;
+                }
+            }
+        }
         
     }
-
+        for (int i = 0; i <num; i++)
+        {
+            if(visited[i]==1)
+            printf("%d\t",i);
+        }
+        
 }
     void main()
     {
-        int num, i = 0, j, k, flag;
+        
+        int num, i = 0, j, k, flag = 1;
+        int graph[max][max] = {0};
         printf("\nEnter the number of vertices in graph:");
         scanf("%d", &num);
-        int graph[num][num] = {0};
         int maxEdge = (num * (num - 1)) / 2;
-        do
+        while (i < maxEdge)
         {
-            printf("\nEnter the vertices connected by edge: ");
+            printf("\nEnter the vertices connected by edge (-1 to exit ): ");
             scanf("%d", &j);
-            printf(",");
+            if(j==-1)
+            break;
             scanf("%d", &k);
-            graph[j][k] = graph[k][k] = 1;
-            printf("\nDo you want to stop,then Enter 0 !!!");
-            scanf("%d", &flag);
-            if (!flag)
-                break;
-        } while (i < maxEdge);
+            if(j>=num || k>=num)
+            {
+                printf("Please enter the vertices b/w 0 and %d",num);
+                continue;
+            }
+            if(j==k)
+            {
+                printf("Please enter two different vertices");
+                continue;
+            }
+            graph[j][k] = graph[k][j] = 1;
+            i++;
+        }
         BFS(graph, num);
     }
