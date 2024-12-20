ab26c47691cf8c725beffe12d9773633719b950d
Subisuresh321 <subisuresh69@gmail.com>
2024-12-20 17:25:00 +0530

    kruskal modified

--- kruskal.c
+++ kruskal.c
@@ -1,41 +1,50 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 #define max 100
-int graph[max][max]={0};
+int A[max][max] = {0};
 
-void main()
+void kruskal(int A[][max],int n)
 {
-    int num,i=0,j,k,weight;
-    printf("\nEnter the number of vertices in graph:");
-    scanf("%d", &num);
-    int queue[num],visited[num];
-    for (int i = 0; i < num; i++)
-        visited[i] = 0;
-    int maxEdge = (num * (num - 1)) / 2;
-    int weightarr[maxEdge];
-    while (i < maxEdge)
+    int min=INT_MAX,minV,minU;
+    for(int i=0;i<n;i++)
     {
-        printf("\nEnter the vertices connected by edge (-1 to exit ): ");
-        scanf("%d", &j);
-        if (j == -1)
-            break;
-        scanf("%d", &k);
-        if (j >= num || k >= num)
-        {
-            printf("Please enter the vertices b/w 0 and %d", num);
-            continue;
-        }
-        if (j == k)
-        {
-            printf("Please enter two different vertices");
-            continue;
-        }
-        printf("\nEnter the weight of Edge:");
-        scanf("%d",&weight);
-        graph[j][k] = graph[k][j] = weight;
-        weightarr[i]=weight;
-        i++;
+       for (int j = 0; j < n; j++) 
+       {
+            if(A[i][j] < min && A[i][j] != INT_MAX)
+            {
+            min=A[i][j];
+            minU=i;
+            minV=j;
+            }
+       }
+    }
+    if(unionFind(minU,minV))
+    {
+
     }
     
-    kruskalAlg();
+}
+
+int unionFind(minU,minV)
+{
+
+}
+void main()
+{
+    int n, i = 0, j, k, weight;
+    printf("\nEnter the number of vertices in graph:");
+    scanf("%d", &n);
+    printf("Enter the adjacency matrix(weight) : \n");
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("A[%d][%d] : ", i, j);
+            scanf("%d", &A[i][j]);
+            if (A[i][j] == 0)
+                A[i][j] = INT_MAX;
+        }
+    }
+    kruskal(A,n);
 }
