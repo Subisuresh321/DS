8968b736b77292420be4e55da2b33bce12369da8
unknown <subisuresh69@gmail.com>
2024-12-23 15:29:36 +0530

    kruskal done

--- kruskal.c
+++ kruskal.c
@@ -1,35 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
-#define max 100
+#define max 10
 int A[max][max] = {0};
 
-void kruskal(int A[][max],int n)
+void Union(int minU,int minV,int parent[],int size[]);
+int Find(int V,int parent[]);
+void Union(int minU,int minV,int parent[],int size[]);
+
+void kruskal(int A[][max], int n)
 {
-    int min=INT_MAX,minV,minU;
-    for(int i=0;i<n;i++)
+    int min, minV, minU,minVP,minUP, edgeCount = 1;
+    int parent[n],size[n];
+    for (int i = 0; i < n; i++){
+        parent[i] = i;
+        size[i]=1;
+    }
+    while (edgeCount < n)
     {
-       for (int j = 0; j < n; j++) 
-       {
-            if(A[i][j] < min && A[i][j] != INT_MAX)
+        min=INT_MAX;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
             {
-            min=A[i][j];
-            minU=i;
-            minV=j;
+                if (A[i][j] < min)
+                {
+                    min = A[i][j];
+                    minU = i;
+                    minV = j; 
+                }
             }
-       }
+        }
+        if(min==INT_MAX)
+        break;
+        A[minU][minV]=A[minV][minU]=INT_MAX;
+        minUP=Find(minU,parent);
+        minVP=Find(minV,parent);
+        if(minUP!=minVP)
+        {
+            printf("%d->",min);
+            Union(minUP,minVP,parent,size);
+            edgeCount++;
+        }
     }
-    if(unionFind(minU,minV))
-    {
-
-    }
-    
 }
 
-int unionFind(minU,minV)
+int Find(int V,int parent[])
 {
-
+    if(parent[V]==V)
+    return V;
+    else{
+    parent[V]=Find(parent[V],parent); //path compression
+    return parent[V];
+    }
 }
+
+void Union(int minUP,int minVP,int parent[],int size[])
+{
+    if(size[minUP]<size[minVP])
+    {
+        parent[minVP]=minUP;
+        size[minUP]+=size[minVP];
+    }
+    else
+    {
+        parent[minUP]=minVP;
+        size[minVP]+=size[minUP];
+    }
+}
+
 void main()
 {
     int n, i = 0, j, k, weight;
@@ -40,11 +79,15 @@ void main()
     {
         for (int j = 0; j < n; j++)
         {
+            if(i==j)
+            A[i][j]=0;
+            else{
             printf("A[%d][%d] : ", i, j);
             scanf("%d", &A[i][j]);
+            }
             if (A[i][j] == 0)
                 A[i][j] = INT_MAX;
         }
     }
-    kruskal(A,n);
+    kruskal(A, n);
 }
